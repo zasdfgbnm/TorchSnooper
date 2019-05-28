c6d2e4941b7c333898dc55caff4a08c4f6d04136
Gao, Xiang <qasdfgtyuiop@gmail.com>
2019-05-28 15:51:32 -0400

    Add support for return_types

--- torchsnooper/__init__.py
+++ torchsnooper/__init__.py
@@ -1,5 +1,6 @@
 import torch
 import pysnooper
+import warnings
 
 
 class TensorFormat:
@@ -60,8 +61,25 @@ class TorchSnooper:
         return type(x).__module__ == 'torch.return_types'
 
     def return_types_repr(self, x):
-        if type(x).__name__ == 'min':
-            return 'min(values=' + self.tensor_format(x.values) + ', indices=' + self.tensor_format(x.indices) + ')'
+        if type(x).__name__ in {'max', 'min', 'median', 'mode', 'sort', 'topk', 'kthvalue'}:
+            return type(x).__name__ + '(values=' + self.tensor_format(x.values) + ', indices=' + self.tensor_format(x.indices) + ')'
+        if type(x).__name__ == 'svd':
+            return 'svd(U=' + self.tensor_format(x.U) + ', S=' + self.tensor_format(x.S) + ', V=' + self.tensor_format(x.V) + ')'
+        if type(x).__name__ == 'slogdet':
+            return 'slogdet(sign=' + self.tensor_format(x.sign) + ', logabsdet=' + self.tensor_format(x.logabsdet) + ')'
+        if type(x).__name__ == 'qr':
+            return 'qr(Q=' + self.tensor_format(x.Q) + ', R=' + self.tensor_format(x.R) + ')'
+        if type(x).__name__ == 'solve':
+            return 'solve(solution=' + self.tensor_format(x.solution) + ', LU=' + self.tensor_format(x.LU) + ')'
+        if type(x).__name__ == 'geqrf':
+            return 'geqrf(a=' + self.tensor_format(x.a) + ', tau=' + self.tensor_format(x.tau) + ')'
+        if type(x).__name__ in {'symeig', 'eig'}:
+            return type(x).__name__ + '(eigenvalues=' + self.tensor_format(x.eigenvalues) + ', eigenvectors=' + self.tensor_format(x.eigenvectors) + ')'
+        if type(x).__name__ == 'triangular_solve':
+            return 'triangular_solve(solution=' + self.tensor_format(x.solution) + ', cloned_coefficient=' + self.tensor_format(x.cloned_coefficient) + ')'
+        if type(x).__name__ == 'gels':
+            return 'gels(solution=' + self.tensor_format(x.solution) + ', QR=' + self.tensor_format(x.QR) + ')'
+        warnings.warn('Unknown return_types encountered, open a bug report!')
 
     def condition(self, x):
         return torch.is_tensor(x) or self.is_return_types(x)
