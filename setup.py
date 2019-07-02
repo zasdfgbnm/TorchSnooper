a5d28a5573eacb98f4a69795d37442125368e678
Gao, Xiang <qasdfgtyuiop@gmail.com>
2019-07-01 20:01:17 -0400

    Add long description as README.md (#10)

--- setup.py
+++ setup.py
@@ -3,11 +3,17 @@
 import setuptools
 
 
+with open("README.md", "r") as fh:
+    long_description = fh.read()
+
+
 setuptools.setup(
     name='TorchSnooper',
     author='Xiang Gao',
     author_email='qasdfgtyuiop@gmail.com',
-    description="A poor man's debugger for PyTorch.",
+    description="Debug PyTorch code using PySnooper.",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
     url='https://github.com/zasdfgbnm/TorchSnooper',
     packages=setuptools.find_packages(exclude=['tests']),
     use_scm_version=True,
